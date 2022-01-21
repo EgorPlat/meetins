24cf345487e5a8b65e3407cb6c3e8d7b1acaf3c6
EgorkaPl <egorplatonov2002@gmail.com>
2022-01-21 14:13:07 +0300

    chageUserStatus

--- pages/profile/About/About.tsx
+++ pages/profile/About/About.tsx
@@ -13,9 +13,10 @@ export default function About(props: {
     const [changingStatus, setChangingStatus] = useState<boolean>(false);
     const [userStatus, setUserStatus] = useState<string>("");
     const authedUser = useStore($user);
-    
+    const isAuthedProfile = JSON.stringify(props.user) === JSON.stringify(authedUser);
+
     const newChangeSatus = (status: boolean) => {  
-        if(JSON.stringify(props.user) === JSON.stringify(authedUser)) {
+        if(isAuthedProfile) {
             setChangingStatus(() => status);
         }
     } 
@@ -24,14 +25,18 @@ export default function About(props: {
     }
     return(
         <div className={s.about}>
+            {authedUser?.status.length! <= 1 && isAuthedProfile
+             ? <p style={{color: "grey"}} onClick={() => newChangeSatus(true)}>Введите Ваш статус...</p>
+             : null
+            }
             {changingStatus
-            ? 
+             ? 
             <div>
                 <textarea autoFocus className={s.textChange} placeholder="Введите текст..." onChange={(event) => setUserStatus(event.target.value)}></textarea>
                 <button className={s.confirmBtn} onClick={saveNewStatus}>ОК</button>
                 <button className={s.cancelBtn} onClick={() => newChangeSatus(false)}>Х</button>
             </div>
-            : <p onClick={() => newChangeSatus(true)}>{props.about}</p>
+             : <p onClick={() => newChangeSatus(true)}>{props.about}</p>
             } 
         </div>
     )
