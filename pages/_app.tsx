bd7b48c32334c793f695cfa25e86458c9df871b1
EgorkaPl <egorplatonov2002@gmail.com>
2022-06-17 10:22:08 +0300

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,6 @@ import { setRouter } from '../global/store/router_model'
 import { io } from 'socket.io-client'
 import { useStore } from 'effector-react'
 
-// commmit
 function MyApp({ Component, pageProps }: AppProps) {
 
 	const router = useRouter();
