9779325c0d69e923d293f99cadd22ae289adc3c7
EgorkaPl <egorplatonov2002@gmail.com>
2022-07-07 13:34:47 +0300

    change effector logic in register and login models

--- global/store/login_model.ts
+++ global/store/login_model.ts
@@ -1,5 +1,7 @@
 import { instance, setUser } from './store'
-import { createEffect, createEvent, createStore } from 'effector'
+import { createEffect, createEvent, createStore, sample } from 'effector'
+import { responseSymbol } from 'next/dist/server/web/spec-compliant/fetch-event'
+import { User } from '../interfaces'
 
 
 type LoginDetailsType = {
@@ -7,7 +9,10 @@ type LoginDetailsType = {
 	password: string 
 } | null 
 
-export const sendLogData = createEffect()
+export const sendLogData = createEffect((async (logDetails: LoginDetailsType) => {
+	const response = await instance.post('auth/login', logDetails)
+	return response;
+}))
 
 export const setLoginDetails = createEvent<LoginDetailsType>()
 export const $loginDetails = createStore<LoginDetailsType>(null).on(
@@ -16,15 +21,13 @@ export const $loginDetails = createStore<LoginDetailsType>(null).on(
 		return newLogDetails
 	}  
 ) 
-
-
-sendLogData.use(async (logDetails) => {
-	const response = await instance.post('auth/login', logDetails)
-	
-	if(response.status === 200) {
-		localStorage.setItem("access-token", response.data.auth.token);	
-		setUser(response.data.profile.user);
-	}
-	console.log(response.status);
-	return response;
+export const saveDataAfterLogin = createEffect((data: any) => {
+	localStorage.setItem('access-token', data.auth.token);
+	setUser(data.profile.user);
+})
+sample({
+	clock: sendLogData.doneData,
+	filter: response => response.status === 200,
+	fn: response => response.data,
+	target: saveDataAfterLogin
 })
