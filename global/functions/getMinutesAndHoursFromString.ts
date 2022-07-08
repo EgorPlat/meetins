00d5d184f9e751ee8286e5a4811671a67190ac24
EgorkaPl <egorplatonov2002@gmail.com>
2022-07-08 11:26:50 +0300

    fixes in view of messageTime

--- global/functions/getMinutesAndHoursFromString.ts
+++ global/functions/getMinutesAndHoursFromString.ts
@@ -1,10 +1,10 @@
 export function getMinutesAndHoursFromString(dateString: string){
     const date = new Date(dateString);
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
+    let hours = String(date.getHours());
+    let minutes = String(date.getMinutes());
 
-    minutes < 10 ? minutes = 0 + minutes : minutes;
-    hours < 10 ? hours = 0 + hours : hours;
+    Number(minutes) < 10 ? minutes = "0" + minutes : minutes;
+    Number(hours) < 10 ? hours = "0" + hours : hours;
 
     return hours + ":" + minutes
 }
