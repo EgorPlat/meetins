1a95c190d65de262e615e9106169923c3a36e0fe
EgorkaPl <egorplatonov2002@gmail.com>
2022-09-06 18:36:39 +0300

    fix-in-event-info-page.

--- pages/eventInfo/EventBlock/index.tsx
+++ pages/eventInfo/EventBlock/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 import Loader from "../../../components/Loader/Loader";
 import { IEventInfoCard } from "../../../global/interfaces/events";
