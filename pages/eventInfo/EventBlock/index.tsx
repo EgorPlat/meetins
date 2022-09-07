d197e2fc838e78e2ab46e771c1d4c1c6a9be562e
EgorkaPl <egorplatonov2002@gmail.com>
2022-09-07 13:14:24 +0300

    fix-in-event-info

--- pages/eventInfo/EventBlock/index.tsx
+++ pages/eventInfo/EventBlock/index.tsx
@@ -5,7 +5,7 @@ import s from "./eventBlock.module.scss";
 
 export default function EventBlock(props: {
     currentEventById: IEventInfoCard
-}): JSX.Element { 
+}): JSX.Element {
     const event = props.currentEventById;
 	if (event) {
         return(
@@ -13,7 +13,7 @@ export default function EventBlock(props: {
                 <div className={s.eventBlockTitle}>Информация про {event.title}</div>
                 <div className={s.eventBlockMainInfo}>
                     <div className={s.image}>
-                        
+                        <img src={event.images[0].image} width={300} height={300} alt="Главное изображение" />
                     </div>
                     <div className={s.help}>
                         <div>{event.title}, {event.age_restriction}+</div>
