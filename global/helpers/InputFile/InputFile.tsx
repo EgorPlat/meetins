40ed968440501eb66f0f3ee2c8a6458c161cdb67
EgorkaPl <egorplatonov2002@gmail.com>
2022-12-09 17:28:19 +0300

    fix-in-inputTypeFile

--- global/helpers/InputFile/InputFile.tsx
+++ global/helpers/InputFile/InputFile.tsx
@@ -1,13 +1,20 @@
-import React, { ChangeEvent, useEffect } from "react";
+import React, { ChangeEvent, useEffect, useRef } from "react";
 import s from "./InputFile.module.scss";
 
 export default function InputFile(props: { onMouseLeave: () => void, onChange: (event: ChangeEvent<HTMLInputElement>) => void }) {
 
+    const ref = useRef(null);
+
+    const emitateInputFileClick = () => {
+        if (ref) {
+            ref.current.click();
+        }
+    }
     return( 
-        <div className={s.formGroup} onMouseLeave={props.onMouseLeave}>
+        <div className={s.formGroup} onClick={emitateInputFileClick} onMouseLeave={props.onMouseLeave}>
+            <span className={s.inputFileTitle}>Выбрать фото</span>
             <label className={s.label}>
-                <div className={s.title}>Добавить файл</div>
-                <input type="file" onChange={(event) => props.onChange(event)} accept=".jpg,.jpeg,.png"/>
+                <input ref={ref} type="file" className={s.inputTypeFile} onChange={(event) => props.onChange(event)} accept=".jpg,.jpeg,.png"/>
             </label>
         </div>
     )
