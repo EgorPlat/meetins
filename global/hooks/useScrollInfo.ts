79aa1d46885697daba0a8cd260ef5a80e25727c9
EgorPlat <egorplatonov2002@gmail.com>
2023-07-12 12:54:07 +0300

    fix-in-view

--- global/hooks/useScrollInfo.ts
+++ global/hooks/useScrollInfo.ts
@@ -5,12 +5,11 @@ export const useScrollDownInfo = (maxPage: number, isClearNeeded: boolean, handl
     const [currentPageNumber, setCurrentPageNumber] = useState(0);
     const [arrayHeights, setArrayHeights] = useState([]);
 
-    const handleScroll = () => {
+    const handleScroll = () => {        
         const isScrollEnded = (window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight - 350;
-        
         const isPageDoesntExists = !arrayHeights.includes(document.body.offsetHeight);
         const isNewPageAreMoreThenMax = arrayHeights.length < maxPage || maxPage === 0;
-
+        
         if (isScrollEnded && isPageDoesntExists && isNewPageAreMoreThenMax) {
             setArrayHeights((lastValue) => [...lastValue, document.body.offsetHeight]);
             setCurrentPageNumber((lastValue) => lastValue + 1);
