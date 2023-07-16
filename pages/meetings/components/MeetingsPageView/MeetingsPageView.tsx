e66f19acbfbfbb6d8632c9cd6c377bd1aba6a8d3
EgorPlat <egorplatonov2002@gmail.com>
2023-07-16 17:52:29 +0300

    add-first-view-of-meetings

--- pages/meetings/components/MeetingsPageView/MeetingsPageView.tsx
+++ pages/meetings/components/MeetingsPageView/MeetingsPageView.tsx
@@ -1,6 +1,6 @@
 import MeetingsContentView from '../MeetingsContentView/MeetingsContentView';
-import MeetingsContent from '../constants';
-import s from './MeetingsPageView.module.scss';
+import MeetingsContent from './constants';
+import s from './MeetingsPage.module.scss';
 
 export default function MeetingsPageView(props: {
     currentMenu: number,
