a1c2c9a06d395747713fdf00db1e3a1ab32a9508
ASPIRIN++ <b.bogomazov@gmail.com>
2024-07-23 21:21:41 +0300

    fix(front): Email regular expression checking is overly strict

--- global/helpers/validate.ts
+++ global/helpers/validate.ts
@@ -1,14 +1,14 @@
 import { addNotification } from "../store/notifications_model"
 
 export const validateEmailOrPhone = (value: string) =>
-	/^(?:\d{11}|\w+@\w+\.\w{2,3})$/.test(value) === false
+	/^(?:\d{11}|\S+@\S+\.\S+)$/.test(value) === false
 		? 'Пожалуйста следуйте формату: user_email@email.com или 79889998889'
 		: true
 export const isPhoneNumber = (value: string | null) => {
 	return value && /\d{11}/.test(value) ? value : null
 }
 export const isEmail = (value: string | null) => {
-	return value && /\w+@\w+\.\w{2,3}/.test(value) ? value : null
+	return value && /\S+@\S+\.\S+/.test(value) ? value : null
 }
 export const isTypeOfFileAreImage = (value: string) => {
 	if (!value) return;
