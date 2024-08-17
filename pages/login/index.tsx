af36a8e61da41fdda50000171b896ce8a7537b6b
EgorPlat <egorplatonov2002@gmail.com>
2024-08-17 15:54:57 +0300

    prohibit-space-symbols-in-password

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,89 +1,100 @@
-import Head from 'next/head';
-import { useForm } from 'react-hook-form';
-import { isEmail } from '../../global/helpers/validate';
-import { $loginLoading, sendLogData } from '../../global/store/login_model';
-import { useState } from 'react';
-import { useStore } from 'effector-react';
-import { useTranslation } from 'react-i18next';
-import Input from '../../components-ui/Input/Input';
-import Link from 'next/link';
-import logo from '../../public/images/logo.svg';
-import Image from 'next/image';
-import CustomLoader from '../../components-ui/CustomLoader/CustomLoader';
-import loginIcon from '../../public/images/login.svg';
-import passIcon from '../../public/images/pass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import s from '../../styles/pageStyles/auth.module.scss';
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+import { isEmail } from "../../global/helpers/validate";
+import { $loginLoading, sendLogData } from "../../global/store/login_model";
+import { useState } from "react";
+import { useStore } from "effector-react";
+import { useTranslation } from "react-i18next";
+import Input from "../../components-ui/Input/Input";
+import Link from "next/link";
+import logo from "../../public/images/logo.svg";
+import Image from "next/image";
+import CustomLoader from "../../components-ui/CustomLoader/CustomLoader";
+import loginIcon from "../../public/images/login.svg";
+import passIcon from "../../public/images/pass.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import s from "../../styles/pageStyles/auth.module.scss";
+import { addNotification } from "../../global/store/notifications_model";
 
 export default function Login(): JSX.Element {
 
-	const { register, handleSubmit, formState: { errors } } = useForm()
-	const [errorMessage, setErrorMessage] = useState<string>("");
-	const loginLoading = useStore($loginLoading);
-	const { t } = useTranslation();
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const loginLoading = useStore($loginLoading);
+    const { t } = useTranslation();
 
-	const sendLoginData = (data: { login: string, password: string }) => {
-		const login = data.login;
-		const pass = data.password;
-		sendLogData({
-			email: login,
-			password: pass,
-		});
-	}
-	return (
-		<div className={s.cardWrapper}>
-			<div className={s.card}>
-				<Head>
-					<title>{t('Вход')}</title>
-					<meta name="keywords" content="meetins, meetin-s, Meetins, Meetin-s, знакомства, meetings, meet" />
-				</Head>
-				<h1 style={{ display: "grid" }}>
-					<Image
-						src={logo}
-					/>
-					{t('Вход')}
-				</h1>
-				<form onSubmit={handleSubmit(sendLoginData)}>
-					<Input
-						icon={loginIcon}
-						placeholder={t('Логин')}
-						type='text'
-						id='login'
-						style={{ marginTop: '22px' }}
-						register={register('login', {
-							required: true,
-							validate: (value) =>
-								isEmail(value) === value
-						})}
-					/>
-					{errors.login && <span className={s.errorSpan}>{t('Введите корректный e-mail в формате *@gmail.com')}</span>}
-					<Input
-						icon={passIcon}
-						placeholder={t('Пароль')}
-						type='password'
-						id='pass'
-						style={{ marginTop: '25px' }}
-						register={register('password', {
-							required: true,
-							validate: (value) =>
-								value.length >= 6 && value.length <= 12
-						})}
-					/>
-					{errors.password && <span className={s.errorSpan}>{t('Не менее 6-ти символов, не более 12-ти символов')}</span>}
-					{errorMessage !== "" ?
-						<div className={`row ${s.errorBlock}`}>
-							{t('Вы ввели неверные данные. Пожалуйста проверьте правильность и попробуйте снова')}
-						</div> : null}
-					<button type='submit' className={`${s.submitBtn} btn`} >
-						{
-							loginLoading ? <CustomLoader /> : t('Войти')
-						}
-					</button>
-					<div className={s.navActions}>
-						<Link href="/register">Еще не зарегистрированы?</Link>
-					</div>
-				</form>
-			</div>
-		</div>
-	)
+    const sendLoginData = (data: { login: string, password: string }) => {
+        const login = data.login;
+        const pass = data.password;
+        const passwordWOSpace = pass.split("").filter(e => e !== " ").join("");
+        if (passwordWOSpace.length === pass.length) {
+            addNotification({
+                text: "В пароле нельзя использовать пробелы",
+                type: "warning",
+                textColor: "black",
+                time: 3000
+            });
+            return;
+        }
+        sendLogData({
+            email: login,
+            password: pass,
+        });
+    }
+    return (
+        <div className={s.cardWrapper}>
+            <div className={s.card}>
+                <Head>
+                    <title>{t("Вход")}</title>
+                    <meta name="keywords" content="meetins, meetin-s, Meetins, Meetin-s, знакомства, meetings, meet" />
+                </Head>
+                <h1 style={{ display: "grid" }}>
+                    <Image
+                        src={logo}
+                    />
+                    {t("Вход")}
+                </h1>
+                <form onSubmit={handleSubmit(sendLoginData)}>
+                    <Input
+                        icon={loginIcon}
+                        placeholder={t("Логин")}
+                        type='text'
+                        id='login'
+                        style={{ marginTop: "22px" }}
+                        register={register("login", {
+                            required: true,
+                            validate: (value) =>
+                                isEmail(value) === value
+                        })}
+                    />
+                    {errors.login && <span className={s.errorSpan}>{t("Введите корректный e-mail в формате *@gmail.com")}</span>}
+                    <Input
+                        icon={passIcon}
+                        placeholder={t("Пароль")}
+                        type='password'
+                        id='pass'
+                        style={{ marginTop: "25px" }}
+                        register={register("password", {
+                            required: true,
+                            validate: (value) =>
+                                value.length >= 6 && value.length <= 12
+                        })}
+                    />
+                    {errors.password && <span className={s.errorSpan}>{t("Не менее 6-ти символов, не более 12-ти символов")}</span>}
+                    {errorMessage !== "" ?
+                        <div className={`row ${s.errorBlock}`}>
+                            {t("Вы ввели неверные данные. Пожалуйста проверьте правильность и попробуйте снова")}
+                        </div> : null}
+                    <button type='submit' className={`${s.submitBtn} btn`} >
+                        {
+                            loginLoading ? <CustomLoader /> : t("Войти")
+                        }
+                    </button>
+                    <div className={s.navActions}>
+                        <Link href="/register">Еще не зарегистрированы?</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,211 +1,222 @@
-import Image from 'next/image'
-import { useForm } from 'react-hook-form'
-import loginIcon from '../../public/images/login.svg'
-import passIcon from '../../public/images/pass.svg'
-import phoneIcon from '../../public/images/phone.svg'
-import femaleIcon from '../../public/images/female.svg'
-import maleIcon from '../../public/images/male.svg'
-import s from '../../styles/pageStyles/auth.module.scss'
-import { useState } from 'react'
-import EyeIcon from '../../global/helpers/Icons/EyeIcon'
-import logo from '../../public/images/logo.svg';
+import Image from "next/image"
+import { useForm } from "react-hook-form"
+import loginIcon from "../../public/images/login.svg"
+import passIcon from "../../public/images/pass.svg"
+import phoneIcon from "../../public/images/phone.svg"
+import femaleIcon from "../../public/images/female.svg"
+import maleIcon from "../../public/images/male.svg"
+import s from "../../styles/pageStyles/auth.module.scss"
+import { useState } from "react"
+import EyeIcon from "../../global/helpers/Icons/EyeIcon"
+import logo from "../../public/images/logo.svg";
 
 import {
-	isEmail,
-	validateEmailOrPhone,
-} from '../../global/helpers/validate'
+    isEmail,
+    validateEmailOrPhone,
+} from "../../global/helpers/validate"
 import {
-	sendRegData,
-	setEmailForConfirmation,
-	setRegisterDetails,
-} from '../../global/store/register_model'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useTranslation } from 'react-i18next'
-import Input from '../../components-ui/Input/Input'
-import Link from 'next/link'
+    sendRegData,
+    setEmailForConfirmation,
+    setRegisterDetails,
+} from "../../global/store/register_model"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { useTranslation } from "react-i18next"
+import Input from "../../components-ui/Input/Input"
+import Link from "next/link"
+import { addNotification } from "../../global/store/notifications_model"
 
 export default function Login(): JSX.Element {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm()
-	const [gender, setGender] = useState<string>('')
-	const [showPassword, setShowPassword] = useState<boolean>(false)
-	const router = useRouter();
-	const { t } = useTranslation();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+    const [gender, setGender] = useState<string>("")
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const router = useRouter();
+    const { t } = useTranslation();
 
-	const sendLoginData = (data: {
+    const sendLoginData = (data: {
 		name: string
 		email: string | null
 		pass: string
 		gender: string
 		city: string
 	} | any) => {
-		const email = isEmail(data.email);
-		const nameArr = data.name.split(' ')
+        const email = isEmail(data.email);
+        const nameArr = data.name.split(" ")
 		
-		setRegisterDetails({
-			name: nameArr[0],
-			email: email,
-			password: data.pass,
-			gender: data.gender,
-			city: data.city
-		})
-		sendRegData({
-			name: nameArr[0],
-			email: email,
-			password: data.pass,
-			gender: data.gender,
-			city: data.city
-		}).then( (res: any) => {
-			if(res.data?.statusCode <= 217) {
-				setEmailForConfirmation(email);
-				router.push(`/confirmation`);
-			}
-		})
-	}
+        const passwordWOSpace = data.password.split("").filter(e => e !== " ").join("");
+        if (passwordWOSpace.length === data.password.length) {
+            addNotification({
+                text: "В пароле нельзя использовать пробелы",
+                type: "warning",
+                textColor: "black",
+                time: 3000
+            });
+            return;
+        }
+        setRegisterDetails({
+            name: nameArr[0],
+            email: email,
+            password: data.pass,
+            gender: data.gender,
+            city: data.city
+        })
+        sendRegData({
+            name: nameArr[0],
+            email: email,
+            password: data.pass,
+            gender: data.gender,
+            city: data.city
+        }).then( (res: any) => {
+            if(res.data?.statusCode <= 217) {
+                setEmailForConfirmation(email);
+                router.push("/confirmation");
+            }
+        })
+    }
 
-	return (
-		<div className={s.cardWrapper}>
-			<div className={s.card}>
-				<Head>
-					<title>Meetins-{t('Регистрация')}</title>
-					<meta name="description" content="Meetins - регистрация" key="desc" />
-					<meta property="og:title" content="Зарегестрируйтесь в Meetins и начните знакомится!" />
-					<meta
-						property="og:description"
-						content="Регистрируйтесь и общайтесь!"
-					/>
-				</Head>
-				<h1 style={{ display:"grid" }}>
-					<Image
-						src={logo}
-					/>
-					{t('Регистрация')}
-				</h1>
-				<form autoComplete='off' onSubmit={handleSubmit(sendLoginData)}>
-					<Input
-						icon={loginIcon}
-						placeholder={t('Имя')}
-						type='text'
-						id='login'	
-						autocomplete={'off'}
-						className={errors.name && s.errorBorder}
-						register={register('name', {
-							required: true,
-							validate: (value) =>
-								/^[a-zа-яё]+$/i.test(value) === false
-									? 'Пожалуйста следуйте формату: Имя'
-									: true,
-						})}
-					/>
-					{errors.name && (
-						<span className={s.errorSpan}>Следуйте формату Имя: </span>
-					)}
-					<Input
-						icon={phoneIcon}
-						placeholder={t('Email')}
-						type='text' 
-						id='phoneOrEmail'
-						style={{ marginTop: '25px' }}
-						className={errors.phone_or_email && s.errorBorder}
-						register={register('email', {
-							required: true,
-							validate: (value) => validateEmailOrPhone(value),
-						})}
-					/>
-					{errors.email && (
-						<span className={s.errorSpan}>{t('Введите валидную почту')}</span>
-					)}
+    return (
+        <div className={s.cardWrapper}>
+            <div className={s.card}>
+                <Head>
+                    <title>Meetins-{t("Регистрация")}</title>
+                    <meta name="description" content="Meetins - регистрация" key="desc" />
+                    <meta property="og:title" content="Зарегестрируйтесь в Meetins и начните знакомится!" />
+                    <meta
+                        property="og:description"
+                        content="Регистрируйтесь и общайтесь!"
+                    />
+                </Head>
+                <h1 style={{ display:"grid" }}>
+                    <Image
+                        src={logo}
+                    />
+                    {t("Регистрация")}
+                </h1>
+                <form autoComplete='off' onSubmit={handleSubmit(sendLoginData)}>
+                    <Input
+                        icon={loginIcon}
+                        placeholder={t("Имя")}
+                        type='text'
+                        id='login'	
+                        autocomplete={"off"}
+                        className={errors.name && s.errorBorder}
+                        register={register("name", {
+                            required: true,
+                            validate: (value) =>
+                                /^[a-zа-яё]+$/i.test(value) === false
+                                    ? "Пожалуйста следуйте формату: Имя"
+                                    : true,
+                        })}
+                    />
+                    {errors.name && (
+                        <span className={s.errorSpan}>Следуйте формату Имя: </span>
+                    )}
+                    <Input
+                        icon={phoneIcon}
+                        placeholder={t("Email")}
+                        type='text' 
+                        id='phoneOrEmail'
+                        style={{ marginTop: "25px" }}
+                        className={errors.phone_or_email && s.errorBorder}
+                        register={register("email", {
+                            required: true,
+                            validate: (value) => validateEmailOrPhone(value),
+                        })}
+                    />
+                    {errors.email && (
+                        <span className={s.errorSpan}>{t("Введите валидную почту")}</span>
+                    )}
 					
-					<Input
-						icon={passIcon}
-						placeholder={t('Пароль')}
-						type='password'
-						id='pass'
-						style={{ marginTop: '25px' }}
-						register={register('pass', {
-							required: true,
-							validate: (value) =>
-								value.length >= 6 && value.length <= 12
-						})}>
-						<EyeIcon
-							onClick={() => {
-								setShowPassword(!showPassword)
-							}}
-							width={25}
-							height={25}
-							style={{ cursor: 'pointer' }}
-						/>
-					</Input>
-					{errors.pass && (
-						<span className={s.errorSpan}>{t('Введите пароль от 6 до 12 символов')}</span>
-					)}
+                    <Input
+                        icon={passIcon}
+                        placeholder={t("Пароль")}
+                        type='password'
+                        id='pass'
+                        style={{ marginTop: "25px" }}
+                        register={register("pass", {
+                            required: true,
+                            validate: (value) =>
+                                value.length >= 6 && value.length <= 12
+                        })}>
+                        <EyeIcon
+                            onClick={() => {
+                                setShowPassword(!showPassword)
+                            }}
+                            width={25}
+                            height={25}
+                            style={{ cursor: "pointer" }}
+                        />
+                    </Input>
+                    {errors.pass && (
+                        <span className={s.errorSpan}>{t("Введите пароль от 6 до 12 символов")}</span>
+                    )}
 
-					<Input
-						icon={phoneIcon}
-						placeholder={t('City')}
-						type='text' 
-						id='city'
-						style={{ marginTop: '25px' }}
-						className={errors.date && s.errorBorder}
-						register={register('city', {
-							required: true,
-						})}
-					/>
-					{errors.city && (
-						<span className={s.errorSpan}>{t('Пожалуйста укажите город')}</span>
-					)}
-					<div className={s.gender}>
-						<span>Выберите пол:</span>
-						<label
-							onClick={() => setGender('male')}
-							className={gender === 'male' ? `${s.checked}` : ''}
-							htmlFor='gender_male'>
-							<Image
-								src={maleIcon}
-								alt='male icon'
-								width={40}
-								height={40}
-							/>
-						</label>
-						<input
-							{...register('gender', { required: true })}
-							id='gender_male'
-							value='male'
-							type='radio'
-						/>
-						<label
-							onClick={() => setGender('female')}
-							className={gender === 'female' ? `${s.checked}` : ''}
-							htmlFor='gender_female'>
-							<Image
-								src={femaleIcon}
-								alt='female icon'
-								width={40}
-								height={40}
-							/>
-						</label>
-						<input
-							{...register('gender', { required: true })}
-							id='gender_female'
-							value='female'
-							type='radio'
-						/>
-					</div>
-					{errors.gender && (
-						<span className={s.errorSpan}>{t('Пожалуйста укажите пол')}</span>
-					)}
-					<button type='submit' className={` btn ${s.submitBtn}`}>
-						{t('Регистрация')}
-					</button>
-					<div className={s.navActions}>
-						<Link href="/login">Уже есть аккаунт?</Link>
-					</div>
-				</form>
-			</div>
-		</div>
-	)
+                    <Input
+                        icon={phoneIcon}
+                        placeholder={t("City")}
+                        type='text' 
+                        id='city'
+                        style={{ marginTop: "25px" }}
+                        className={errors.date && s.errorBorder}
+                        register={register("city", {
+                            required: true,
+                        })}
+                    />
+                    {errors.city && (
+                        <span className={s.errorSpan}>{t("Пожалуйста укажите город")}</span>
+                    )}
+                    <div className={s.gender}>
+                        <span>Выберите пол:</span>
+                        <label
+                            onClick={() => setGender("male")}
+                            className={gender === "male" ? `${s.checked}` : ""}
+                            htmlFor='gender_male'>
+                            <Image
+                                src={maleIcon}
+                                alt='male icon'
+                                width={40}
+                                height={40}
+                            />
+                        </label>
+                        <input
+                            {...register("gender", { required: true })}
+                            id='gender_male'
+                            value='male'
+                            type='radio'
+                        />
+                        <label
+                            onClick={() => setGender("female")}
+                            className={gender === "female" ? `${s.checked}` : ""}
+                            htmlFor='gender_female'>
+                            <Image
+                                src={femaleIcon}
+                                alt='female icon'
+                                width={40}
+                                height={40}
+                            />
+                        </label>
+                        <input
+                            {...register("gender", { required: true })}
+                            id='gender_female'
+                            value='female'
+                            type='radio'
+                        />
+                    </div>
+                    {errors.gender && (
+                        <span className={s.errorSpan}>{t("Пожалуйста укажите пол")}</span>
+                    )}
+                    <button type='submit' className={` btn ${s.submitBtn}`}>
+                        {t("Регистрация")}
+                    </button>
+                    <div className={s.navActions}>
+                        <Link href="/login">Уже есть аккаунт?</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
