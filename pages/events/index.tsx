9fc15d49918253e74bea680ba480fa369626d0f4
Egor <49120293+EgorPlat@users.noreply.github.com>
2024-08-24 14:03:45 +0300

    Update index.tsx

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,6 +1,6 @@
 import PageContainer from "../../global/components/PageContainer/pageContainer";
 import React from "react";
-import EventsPageView from "./components/EventsPageView/eventsPageView";
+import EventsPageView from "./components/EventsPageView/EventsPageView";
 
 export default function Events(): JSX.Element {
 
