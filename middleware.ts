b13dcf2f401d5ba334c91d580a015b7d5a249d44
EgorPlat <egorplatonov2002@gmail.com>
2024-09-17 08:24:16 +0300

    add middleware.ts to check user auth token in cookies.

--- middleware.ts
+++ middleware.ts
@@ -7,10 +7,10 @@ export async function middleware(req: NextRequest) {
     if (url.match(/\.(css|js|png|jpg|jpeg|gif|ico)$/) || url.includes("/_next/")) {
         return NextResponse.next();
     }
-    if (token && token.value) {
+    else if (token && token.value) {
         return NextResponse.next();
     }
-    if (url.includes("/login") || url.includes("/register") || url.includes("/confirmation")) {
+    else if (url.includes("/login") || url.includes("/register") || url.includes("/confirmation")) {
         return NextResponse.next();
     }
     else {
