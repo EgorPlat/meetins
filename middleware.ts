603d933aa1ec0badd888db6176b00b5976c1cf6c
EgorPlat <egorplatonov2002@gmail.com>
2024-09-17 08:10:18 +0300

    add middleware.ts to check user auth token in cookies

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const { cookies, url } = req;
+    const token = cookies.get("access_token");
+    
+    if (url.match(/\.(css|js|png|jpg|jpeg|gif|ico)$/) || url.includes("/_next/")) {
+        return NextResponse.next();
+    }
+    if (url.includes("/login") || url.includes("/register")) {
+        return NextResponse.next();
+    }
+    if (token && token.value) {
+        return NextResponse.next();
+    }
+    else if (url.includes("/login")) {
+        return NextResponse.next();
+    }
+    else {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+}
+
+export const config = {
+    matcher: [
+        "/:path*",
+    ],
+};
