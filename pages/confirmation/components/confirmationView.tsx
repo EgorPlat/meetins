fee2fe07864d2a67bd943590a7686c20f94fb535
EgorPlat <egorplatonov2002@gmail.com>
2024-09-26 10:11:03 +0300

    add hint for confirmarion page

--- pages/confirmation/components/confirmationView.tsx
+++ pages/confirmation/components/confirmationView.tsx
@@ -4,6 +4,8 @@ import logo from "../../../public/images/logo.svg";
 import s from "./confirmation.module.scss";
 import Link from "next/link";
 import CustomSplitedInput from "../../../shared/ui/CustomSplitedInput/CustomSplitedInput";
+import CustomButton from "../../../shared/ui/CustomButton/CustomButton";
+import CustomButtonWithHint from "../../../shared/ui/CustomButtonWithHint/CustomButtonWithHint";
 
 interface IConfirmationViewProps {
     sendConfirmationCode: (code: number) => void
@@ -22,11 +24,17 @@ export default function ConfirmationView({ sendConfirmationCode }: IConfirmation
                 На указанную Вами при регистрации почту был выслан код подтверждения.
                 Пожалуйста впишите его в поле и нажмите "Отправить"
             </div>
+            <CustomButtonWithHint
+                title="Не получили код ?"
+                fontSize={16}
+                hintTitle="Если Вы не получили код, пожалуйста, проверьте папку спам или убедитесь, что Ваш почтовый ящик не переполнен."
+            />
             <div className={s.content}>
                 <CustomSplitedInput count={6} handleChangeValue={value => setCode(+value)} />
             </div>
+
             <div className={s.actions}>
-                <button onClick={() => sendConfirmationCode(code)}>Отправить</button>
+                <CustomButton text="Отправить" onClick={() => sendConfirmationCode(code)} />
                 <Link href="/login" className={s.goBack}>Вернуться назад</Link>
             </div>
         </div>
