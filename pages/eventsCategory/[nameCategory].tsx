ed3fbd9418df35c81a9d6c8a3044718587673d54
EgorPlat <egorplatonov2002@gmail.com>
2024-10-01 09:26:30 +0300

    add ssr for events category page

--- pages/eventsCategory/[nameCategory].tsx
+++ pages/eventsCategory/[nameCategory].tsx
@@ -1,41 +1,43 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react";
-import { currentEvents, getEvents, loadedStatus, setLoadedStatus } from "../../global/store/events_model";
-import { useUnit } from "effector-react";
+import { setCurrentEvents } from "../../global/store/events_model";
 import { getCategoryName } from "../../shared/helpers/helper";
 import PageContainer from "../../widgets/PageContainer/pageContainer";
 import EventsList from "./components/EventsList/EventsList";
+import { GetServerSideProps } from "next";
+import { instance } from "../../global/store/store";
 
-export default function CategoryEventInfo(): JSX.Element {
+export default function CategoryEventInfo({ events }): JSX.Element {
 
     const router = useRouter();
     const { nameCategory } = router.query;
-    
-    const currentEvents$ = useUnit(currentEvents);
-    const loadedStatus$ = useUnit(loadedStatus);
     const categoryName = getCategoryName(nameCategory?.toString());
 
     useEffect(() => {
-        getEvents({categoryName: nameCategory?.toString(), page: 1});
-    }, [nameCategory]);
-
-    useEffect(() => {
-        return () => {
-            setLoadedStatus(false);
-        }
-    }, []);
+        setCurrentEvents(events);
+    }, [events]);
     
     return (
         <PageContainer>
             {
-                nameCategory && typeof nameCategory === "string"
-                    ? 
-                    <EventsList 
-                        categoryName={categoryName}
-                        currentEvents={currentEvents$}
-                        loadedStatus={loadedStatus$}
-                    /> : null
+                <EventsList 
+                    categoryName={categoryName}
+                    currentEvents={events}
+                    loadedStatus={true}
+                />
             }
         </PageContainer>
     )
 }
+
+export const getServerSideProps = (async (context) => {
+
+    const categoryName = context.query.nameCategory;
+    const response = await instance.post(
+        "event/getEventsCategory", { nameCategory: categoryName, page: 1 }
+    );
+    
+    return { 
+        props: { events: response.data } 
+    }
+}) satisfies GetServerSideProps<{ events }>
