060dcf126e27db5d6523b8100d12cffcc95721d8
EgorPlat <egorplatonov2002@gmail.com>
2024-10-03 10:01:50 +0300

    remove unused interface elements from music page

--- pages/music/components/MusicPageView/MusicPageView.tsx
+++ pages/music/components/MusicPageView/MusicPageView.tsx
@@ -42,7 +42,6 @@ export default function MusicPageView({
                     placeholder={t("Введите псевдоним автора")}
                     onChange={(e) => setSearchMusic(e.target.value)}
                 />
-                <button className={s.musicSearchBtn}>{t("Искать")}</button>
             </div>
             <div className={s.musicContent}>
                 <div className={s.musicList}>
